dabb0a9243139aa07639d69d1415d437ca1356da
Pavel Hermann <hermann.pawel966@gmail.com>
2025-02-08 11:07:51 +0100

    Add properties to message dto

--- ChatAppBackend/DTOs/MessageDto.cs
+++ ChatAppBackend/DTOs/MessageDto.cs
@@ -4,5 +4,13 @@ namespace ChatAppBackend.DTOs;
 
 public class MessageDto
 {
+	public int Id { get; set; }
+	public string Content { get; set; } = string.Empty;
 
+	public DateTime? TimeStamp { get; set; }
+
+	public DateTime? DeleteDate { get; set; }
+
+	public int UserId { get; set; }
+	public int ChatId { get; set; }
 }
